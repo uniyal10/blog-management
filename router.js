7392852873dc065215b2de759d79ed8b98b6b8ed
SUDHANSHU UNIYAL <uniyal1001@gmail.com>
2020-07-21 23:00:04 +0530

    Update router.js
    optimize code

--- router.js
+++ router.js
@@ -1,37 +1,17 @@
 const apiRouter = require("express").Router()
-
 const postController = require("./controllers/postController")
-
 const cors = require("cors")
 
 apiRouter.use(cors())
 
 apiRouter.get("/", (req, res) => res.json("Hello, if you see this message that means your backend is up and running successfully. Congrats! Now let's continue learning React!"))
 
-router.post("/add", contactController.add)
-router.get("/", (req, res) => {
-  userController.find().toArray((err, list) => {
-    if (err) {
-      console.log("something wrong")
-    } else {
-      res.send(list)
-    }
-  })
-})
 
-router.post("/edit", (req, res) => {
-  userController.findOneAndUpdate({ _id: new mongodb.ObjectID(req.body.id) }, { $set: { name: req.body.name, date: req.body.date, number: req.body.number, email: req.body.email } }, (err, data) => {
-    if (err) {
-      console.log("something wrong")
-    } else {
-      console.log("update sucessfully")
-      res.redirect("/")
-    }
-  })
-})
-router.post("/delete", (req, res) => {
-  userController.deleteOne({ _id: new mongodb.ObjectID(req.body.id) }, (err, data) => {
-    res.send("sucessfull")
-  })
-})
+apiRouter.get("/post/:id", postController.reactApiViewSingle)
+apiRouter.post("/post/:id/edit", userController.apiMustBeLoggedIn, postController.apiUpdate)
+apiRouter.delete("/post/:id", userController.apiMustBeLoggedIn, postController.apiDelete)
+apiRouter.post("/create-post", userController.apiMustBeLoggedIn, postController.apiCreate)
+apiRouter.post("/search", postController.search)
+
+
 module.exports = apiRouter
