e595e1b3d3729acdc0a44e123dd1067e0470c265
SUDHANSHU UNIYAL <uniyal1001@gmail.com>
2020-07-21 23:11:26 +0530

    Update router.js
    final commit

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 
 apiRouter.use(cors())
 
-apiRouter.get("/", (req, res) => res.json("Hello, if you see this message that means your backend is up and running successfully. Congrats! Now let's continue learning React!"))
+apiRouter.get("/", (req, res) => res.json("blog management running..."))
 
 
 apiRouter.get("/post/:id", postController.reactApiViewSingle)
