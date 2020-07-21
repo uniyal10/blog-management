2cebdc01ca64f2184d33f904b55fd0ec5b74fbb0
sudhanshu uniyal <uniyal1001@gmail.com>
2020-07-21 15:56:01 +0530

    added post controller

--- router.js
+++ router.js
@@ -1,16 +1,11 @@
 const apiRouter = require("express").Router()
-const userController = require("./controllers/userController")
+
 const postController = require("./controllers/postController")
+
 const cors = require("cors")
 
 apiRouter.use(cors())
 
 apiRouter.get("/", (req, res) => res.json("Hello, if you see this message that means your backend is up and running successfully. Congrats! Now let's continue learning React!"))
-
-apiRouter.get("/post", postController.getAllPosts)
-apiRouter.post("/post/:id/edit", postController.apiUpdate)
-apiRouter.delete("/post/:id", postController.apiDelete)
-apiRouter.post("/create-post", postController.apiCreate)
-apiRouter.post("/search", postController.search)
 
 module.exports = apiRouter
